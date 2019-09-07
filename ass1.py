e8bbb24e520dff5c0a09026e71f667153007fed9
Michael <michael.j.lukiman@gmail.com>
2019-09-06 20:41:09 -0400

    Init

---
+++ ass1.py
@@ -0,0 +1,23 @@
+import torch
+
+
+def load_up_2d_dataset():
+    file_path = "assign0_data.py"
+    data = torch.load(file_path)
+
+    print(str(data))
+
+def visualize_2d_scatter_plot():
+    pass
+
+def translate_to_zero_mean():
+    pass
+
+def decorrelate_covariance_to_identity():
+    pass
+
+def plot_whitened_data():
+    pass
+
+def discuss_dependencies():
+    pass
